d48561cc4fb78fdebe87f633a04239bf6dcae7c3
Adam Bodnar <adam.p.bodnar@gmail.com>
2015-07-01 18:24:00 -0700

    simple fbo stuff

--- fspt.js
+++ fspt.js
@@ -1,12 +1,14 @@
 var gl;
-var squareBuffer;
-var noiseTex;
 var program;
+var squareBuffer;
+var textures = []
+var noiseTex;
+var framebuffers = [];
 
 function initGL(canvas) {
   gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-  gl.viewportWidth = canvas.width = 512;//window.innerWidth;
-  gl.viewportHeight = canvas.height = 512;//window.innerHeight;
+  gl.viewportWidth = canvas.width = window.innerWidth;
+  gl.viewportHeight = canvas.height = window.innerHeight;
 }
 
 function getShader(gl, id) {
@@ -39,24 +41,41 @@ function getShader(gl, id) {
   return shader;
 }
 
-function initTexture() {
-  var b = new ArrayBuffer(gl.viewportWidth*gl.viewportHeight*4);
-  var v1 = new Uint8Array(b);
-  
+function initTexture(){
+  var b = new ArrayBuffer(gl.viewportWidth*gl.viewportHeight*4*4);
+  var v1 = new Float32Array(b);
+
   for ( var i=0; i<gl.viewportWidth*gl.viewportHeight*4; i+=4 ){
-    v1[i] = Math.floor((Math.random()*255)+1);
-    v1[i+1] = Math.floor((Math.random()*255)+1);
-    v1[i+2] = Math.floor((Math.random()*255)+1);
-    v1[i+3] = 255;
+    v1[i] =   Math.random();
+    v1[i+1] = Math.random();
+    v1[i+2] = Math.random();
+    v1[i+3] = 1;
   }
-  noiseTex = gl.createTexture () ;
-  gl.activeTexture(gl.TEXTURE0);
-  gl.bindTexture( gl.TEXTURE_2D, noiseTex ) ;
-  gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR ) ;
-  gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR ) ;
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.viewportWidth, gl.viewportHeight, 0, gl.RGBA, gl.UNSIGNED_BYTE, v1);
-  gl.generateMipmap( gl.TEXTURE_2D ) ;
-  gl.uniform1i(program.texUniform, 0);
+
+  noiseTex = createTexture();
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.viewportWidth, gl.viewportHeight, 0, gl.RGBA, gl.FLOAT, v1);
+}
+
+function createTexture() {
+  var t = gl.createTexture () ;
+  gl.getExtension('OES_texture_float');
+  //gl.getExtension('OES_texture_float_linear');
+  gl.bindTexture( gl.TEXTURE_2D, t ) ;
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST ) ;
+  gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST ) ;
+  gl.texImage2D(
+    gl.TEXTURE_2D, 0, gl.RGBA, gl.viewportWidth, gl.viewportHeight, 0,
+    gl.RGBA, gl.UNSIGNED_BYTE, null);
+  return t;
+}
+
+function createFramebuffer(tex){
+  var fbo = gl.createFramebuffer();
+  gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+  gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, tex, 0);
+  return fbo;
 }
 
 function initShaders() {
@@ -75,7 +94,8 @@ function initShaders() {
   gl.useProgram(program);
   program.vertexAttribute = gl.getAttribLocation(program, "corner");
   gl.enableVertexAttribArray(program.vertexAttribute);
-  gl.getUniformLocation(program, "tex")
+  program.texUniform = gl.getUniformLocation(program, "tex")
+  program.modeLocation = gl.getUniformLocation(program, "mode");
 }
 
 function initBuffers(){
@@ -89,11 +109,23 @@ function initBuffers(){
   ];
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
   gl.vertexAttribPointer(program.vertexAttribute, 3, gl.FLOAT, false, 0, 0);
+
+  var tex = createTexture();
+  textures.push(createTexture());
+  textures.push(createTexture());
+  framebuffers.push(createFramebuffer(textures[0]));
+  framebuffers.push(createFramebuffer(textures[1]));
 }
 
 function drawScene() {
   gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.bindTexture(gl.TEXTURE_2D, noiseTex);
+  gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffers[0]);
+  gl.uniform1i(program.modeLocation, 0);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+  gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+  gl.bindTexture(gl.TEXTURE_2D, textures[0]);
+  gl.uniform1i(program.modeLocation, 1);
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 }
 
@@ -108,9 +140,9 @@ function webGLStart() {
   initShaders();
   initBuffers();
   initTexture();
-  
+
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.enable(gl.DEPTH_TEST);
-  
+  gl.disable(gl.BLEND);
+
   tick();
 }
