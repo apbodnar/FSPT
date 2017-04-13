c9a8b1ca4fe7f18bc8165e5e06ada21ba0066af0
Adam Bodnar <adam.p.bodnar@gmail.com>
2017-04-12 20:29:18 -0700

    asdf

--- primitive_tree.js
+++ primitive_tree.js
@@ -1,11 +1,10 @@
 function BVH(objText) {
-  this.tree = Node(triangles)
+  var triangles = parseMesh()
+  this.tree = new Node(triangles)
 
   function build(parent) {
-    var triangles = parseMesh()
     var xSorted = [...Array(triangles.length).keys()]
-    console.log(triangles)
-  }.bind(this)
+  }
 
   function splitNode(){}
 
@@ -20,7 +19,11 @@ function BVH(objText) {
       if(array[0] == 'v'){
         vertices.push(vals)
       } else if (array[0] == 'f') {
-        var tri = new Triangle(vertices[vals[0]], vertices[vals[1]], vertices[vals[2]])
+        var tri = new Triangle (
+          vertices[vals[0]-1],
+          vertices[vals[1]-1],
+          vertices[vals[2]-1]
+        )
         triangles.push(tri)
       }
     }
@@ -29,36 +32,59 @@ function BVH(objText) {
   build(this.tree)
 }
 
+class BoundingBox {
+  constructor(triangles) {
+    var tris  = Array.isArray(triangles) ? triangles : [triangles]
+    this._box = [Infinity, -Infinity, Infinity, -Infinity, Infinity, -Infinity]
+    for(var j=0; j<tris.length; j++){
+      var vals = tris[j].v1.concat(tris[j].v2, tris[j].v3);
+      for(var i = 0; i < vals.length; i++){
+        this._box[(i % 3) * 2] = Math.min(vals[i], this._box[(i % 3) * 2]);
+        this._box[(i % 3) * 2 + 1] = Math.max(vals[i], this._box[(i % 3) * 2 + 1]);
+      }
+    }
+    this._centroid = [
+      (this._box[0]+this._box[1])/2,
+      (this._box[2]+this._box[3])/2,
+      (this._box[4]+this._box[5])/2
+    ]
+  }
+
+  getBounds() {
+    return this._box;
+  }
+
+  getCenter() {
+    return this._centroid;
+  }
+}
+
 function Node(triangles){
+  this.triangles = triangles
+  this.boundingBox = new BoundingBox(triangles)
   this.leaf = false;
   this.left = null;
   this.right = null;
+  function getSplitingAxis(){
+    var box = this.boundingBox.getBounds()
+    var bestIndex = 0;
+    var bestSpan = 0;
+    for(var i=0; i < box.length / 2 ; i++){
+      if(box[i*2])
+    }
+  }
 }
 
 function Triangle(v1, v2, v3) {
   this.v1 = v1;
   this.v2 = v2;
   this.v3 = v3;
-  this.boundingBox = function(){
-    var vals = v1.concat(v2, v3);
-    var box = [Infinity, -Infinity, Infinity, -Infinity, Infinity, -Infinity]
-    for(var i = 0; i < vals.length; i++){
-      var axis = vals[i]
-      box[(i % 3) * 2] = Math.min(axis, box[(i % 3) * 2])
-      box[(i % 3) * 2 + 1] = Math.max(axis, box[(i % 3) * 2 + 1])
-    }
-    return {x: [box[0],box[1]], y: [box[2],box[3]], z: [box[4],box[5]]};
-  }.call(this)
-  this.centroid = function(){
-    var box = this.boundingBox;
-    return {x: (box.x[0]+box.x[1])/2,
-            y: (box.y[0]+box.y[1])/2,
-            z: (box.z[0]+box.z[1])/2}
-  }.call(this)
+  this.boundingBox = new BoundingBox(this)
 }
 
 var bvh;
 
 loadAll(['mesh/bunny.obj'], function(hash){
   bvh = new BVH(hash['mesh/bunny.obj'])
+  console.log(bvh)
 })
