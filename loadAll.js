6cb83fa01fa09a666a964d7049c5050221bfd430
Adam Bodnar <adam.p.bodnar@gmail.com>
2017-06-16 17:57:56 -0700

    reformat, refactor, optimize

--- loadAll.js
+++ loadAll.js
@@ -1,40 +1,44 @@
-(function(exports){
-  exports.loadAll = function(urls, callback){
+(function (exports) {
+  exports.loadAll = function (urls, callback) {
     var counter = urls.length;
     var resHash = {};
-    urls.forEach(function(url){
-      if(url.match(/^texture\//)){
+    urls.forEach(function (url) {
+      if (url.match(/^texture\//)) {
         img = new Image();
-        img.onload = function(){
+        img.onload = function () {
           counter--;
           resHash[url] = img;
-          if(counter === 0){
-            callback.apply(null,[resHash]);
+          if (counter === 0) {
+            callback.apply(null, [resHash]);
           }
         };
         img.src = url;
       } else {
         var req = new XMLHttpRequest();
-        req.addEventListener("load", function(res){
+        req.addEventListener("load", function (res) {
           counter--;
           resHash[url] = res.target.responseText;
-          if(counter === 0){
-            callback.apply(null,[resHash]);
+          if (counter === 0) {
+            callback.apply(null, [resHash]);
           }
         });
-        req.addEventListener("error", function(){ counter = -1; });
+        req.addEventListener("error", function () {
+          counter = -1;
+        });
         req.open("GET", url, true);
         req.send();
       }
     });
   }
 
-  exports.getText = function(path, callback){
+  exports.getText = function (path, callback) {
     var req = new XMLHttpRequest();
-    req.addEventListener("load", function(res){
-      callback.apply(null,[res.target.responseText]);
+    req.addEventListener("load", function (res) {
+      callback.apply(null, [res.target.responseText]);
     });
-    req.addEventListener("error", function(){ counter = -1; });
+    req.addEventListener("error", function () {
+      counter = -1;
+    });
     req.open("GET", path, true);
     req.send();
   }
--- vector.js
+++ vector.js
@@ -1,91 +1,95 @@
 /**
  * Created by adam on 4/21/17.
  */
+(function (exports) {
+  class Vec3 {
+    static magnitude(v) {
+      return Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2])
+    }
 
-function magnitude(v){
-  return Math.sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2])
-}
+    static normalize(v) {
+      var m = Vec3.magnitude(v);
+      return Vec3.scale(v, 1 / m);
+    }
 
-function normalize(v){
-  var m = magnitude(v);
-  return scale(v, 1/m);
-}
+    static scale(v, s) {
+      return [v[0] * s, v[1] * s, v[2] * s];
+    }
 
-function scale(v, s){
-  return [v[0]*s, v[1]*s, v[2]*s];
-}
+    static add(v1, v2) {
+      return [v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]];
+    }
 
-function add(v1, v2){
-  return [v1[0]+v2[0],v1[1]+v2[1],v1[2]+v2[2]];
-}
+    static sub(v1, v2) {
+      return [v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]];
+    }
 
-function sub(v1, v2){
-  return [v1[0]-v2[0],v1[1]-v2[1],v1[2]-v2[2]];
-}
+    static inverse(v) {
+      return [1.0 / v[0], 1.0 / v[1], 1.0 / v[2]];
+    }
 
-function inverse(v){
-  return [1.0/v[0], 1.0/v[1], 1.0/v[2]];
-}
+    static dot(v1, v2) {
+      return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
+    }
 
-function dot(v1, v2){
-  return v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2];
-}
+    static rotateX(v, a) {
+      var x = v[0],
+        y = v[1],
+        z = v[2];
+      var x1 = x,
+        y1 = z * Math.sin(a) + y * Math.cos(a),
+        z1 = z * Math.cos(a) - y * Math.sin(a);
+      return [x1, y1, z1];
+    }
 
-function rotateX(v, a){
-  var x = v[0],
-    y = v[1],
-    z = v[2];
-  var x1 = x,
-    y1 = z*Math.sin(a) + y*Math.cos(a),
-    z1 = z*Math.cos(a) - y*Math.sin(a);
-  return [x1,y1,z1];
-}
+    static rotateY(v, a) {
+      var x = v[0],
+        y = v[1],
+        z = v[2];
+      var y1 = y,
+        x1 = z * Math.sin(a) + x * Math.cos(a),
+        z1 = z * Math.cos(a) - x * Math.sin(a);
+      return [x1, y1, z1];
+    }
 
-function rotateY(v, a){
-  var x = v[0],
-    y = v[1],
-    z = v[2];
-  var y1 = y,
-    x1 = z*Math.sin(a) + x*Math.cos(a),
-    z1 = z*Math.cos(a) - x*Math.sin(a);
-  return [x1,y1,z1];
-}
+    static rotateZ(v, a) {
+      var x = v[0],
+        y = v[1],
+        z = v[2];
+      var z1 = z,
+        x1 = y * Math.sin(a) + x * Math.cos(a),
+        y1 = y * Math.cos(a) - x * Math.sin(a);
+      return [x1, y1, z1];
+    }
 
-function rotateZ(v, a){
-  var x = v[0],
-    y = v[1],
-    z = v[2];
-  var z1 = z,
-    x1 = y*Math.sin(a) + x*Math.cos(a),
-    y1 = y*Math.cos(a) - x*Math.sin(a);
-  return [x1,y1,z1];
-}
-
-function rotateArbitrary(v, axis, angle){
-  var x = axis[0],
-      y = axis[1],
-      z = axis[2];
-  var s = Math.sin(angle);
-  var c = Math.cos(angle);
-  var oc = 1.0 - c;
-  var mat = [oc * x * x + c, oc * x * y - z * s, oc * z * x + y * s,
+    static rotateArbitrary(v, axis, angle) {
+      var x = axis[0],
+        y = axis[1],
+        z = axis[2];
+      var s = Math.sin(angle);
+      var c = Math.cos(angle);
+      var oc = 1.0 - c;
+      var mat = [oc * x * x + c, oc * x * y - z * s, oc * z * x + y * s,
         oc * x * y + z * s, oc * y * y + c, oc * y * z - x * s,
         oc * z * x - y * s, oc * y * z + x * s, oc * z * z + c];
-  return matVecMultiply(v, mat);
-}
+      return Vec3.matVecMultiply(v, mat);
+    }
 
-function matVecMultiply(vec, mat){
-  var res = [];
-  for(var i=0; i<3; i++){
-    var row = [mat[3*i], mat[3*i+1], mat[3*i+2]];
-    res.push(dot(row, vec));
+    static matVecMultiply(vec, mat) {
+      var res = [];
+      for (var i = 0; i < 3; i++) {
+        var row = [mat[3 * i], mat[3 * i + 1], mat[3 * i + 2]];
+        res.push(Vec3.dot(row, vec));
+      }
+      return res;
+    }
+
+    static cross(v1, v2) {
+      var x = v1[1] * v2[2] - v1[2] * v2[1],
+        y = -(v1[0] * v2[2] - v1[2] * v2[0]),
+        z = v1[0] * v2[1] - v1[1] * v2[0];
+      return [x, y, z];
+    }
   }
-  return res;
-}
-
-function cross(v1, v2){
-  var x = v1[1] * v2[2] - v1[2] * v2[1],
-    y = -(v1[0] * v2[2] - v1[2] * v2[0]),
-    z = v1[0] * v2[1] - v1[1] * v2[0];
-  return [x, y, z];
-}
+  exports.Vec3 = Vec3;
+})(this);
